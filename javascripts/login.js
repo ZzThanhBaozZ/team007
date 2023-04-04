e2ff903f0b857f788be1c626e854512bc2c03a49
Kim Anh <kimanhtranhoang@gmail.com>
2023-04-05 02:42:44 +0700

    modify index.css and login.js

--- javascripts/login.js
+++ javascripts/login.js
@@ -62,28 +62,6 @@ togglePassword.addEventListener("click", function () {
     this.classList.toggle("fa-eye");
 });
 
-// Remove a Recent login item 
-// const removeButtons = document.querySelectorAll('.recent-login__item-remove');
-
-// removeButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-    
-//     const parent = button.parentNode;
-    
-//     parent.style.opacity = 0;
-//     parent.style.transition = 'opacity 0.2s ease-in-out';
-
-//     setTimeout(() => {
-//         parent.remove();
-
-//         const nextSibling = parent.nextElementSibling;
-//         if (nextSibling) {
-//         nextSibling.style.animation = 'slide 0.5s ease-in forwards';
-//         }
-//     }, 500);
-//     });
-// });
-
 // Hide Recent accounts and Show Login Form only when clicking Add 
 const addLoginItem = document.querySelector('.recent-login__item-add');
 const firstWrapper = document.querySelector('.first-wrapper');
