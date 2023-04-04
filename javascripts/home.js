5129eebbb26f58f98bb304a65dc76c7ff5ba05b1
Kim Anh <kimanhtranhoang@gmail.com>
2023-04-04 14:02:11 +0700

    fix height wrappers home

--- javascripts/home.js
+++ javascripts/home.js
@@ -47,3 +47,18 @@ perIcon.addEventListener('click', function() {
 
   perList.classList.toggle('show');
 });
+
+// Show hamburger when clicking
+const hamburgerMenu = document.querySelector('#ham-d');
+const menuContent = document.querySelector('.menu-content');
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  menuContent.classList.toggle('active');
+});
+const hamburgerMenuM = document.querySelector('#ham-m');
+
+hamburgerMenuM.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  menuContent.classList.toggle('active');
+});
